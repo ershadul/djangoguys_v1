e425a068244ee13e622727660f52ccf7cfd6ae70
Ershadul <ershadul@Ershaduls-Mac.local>
2011-08-02 22:45:23 +0600

    Refactored home page and added archive page

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from django.conf import settings
 
 urlpatterns = patterns('posts.views',
     url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>.*)', 'show_post'),
+	url(r'^archive', 'archive'),
     url(r'^$', 'index'),
 )
--- posts/views.py
+++ posts/views.py
@@ -7,8 +7,12 @@ from posts.models import Post
 def show_post(request, year, month, day, slug):
     post = Post.objects.filter(is_published=True).get(slug=slug)
 
-    return render_to_response('show_post.html', {'post': post})
+    return render_to_response('show_post.html', {'post': post, 'menu_index': -1 })
 
 def index(request):
+    posts = Post.objects.filter(is_published=True).order_by('-pub_date').all()[0:5]
+    return render_to_response('index.html', {'posts': posts, 'menu_index': 0 })
+
+def archive(request):
     posts = Post.objects.filter(is_published=True).order_by('-pub_date').all()
-    return render_to_response('index.html', {'posts': posts})
+    return render_to_response('archive.html', {'posts': posts, 'menu_index': 1 })
